0257763caf0cf25804f2eb3de57a36f48147a312
Olu Ayandosu <oayandosu@gmail.com>
2012-04-06 14:58:09 -0700

    Change port to support Heroku environment port

--- server.js
+++ server.js
@@ -43,5 +43,9 @@ app.post('/snap', function(req, res) {
 function nameFile() {
 }
 
-app.listen(3000);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
