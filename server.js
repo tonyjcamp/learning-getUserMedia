22fa39d0c33dd4fc75849a24b7e126298f943f43
Olu Ayandosu <oayandosu@gmail.com>
2012-04-06 14:55:57 -0700

    Fresh build

--- server.js
+++ server.js
@@ -39,5 +39,9 @@ app.post('/snap', function(req, res) {
 	});
 });
 
+
+function nameFile() {
+}
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
