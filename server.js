4199f6a215b7a8b597a06f30781afed738bb5d00
Olu Ayandosu <oayandosu@gmail.com>
2012-04-06 17:24:18 -0700

    Add twitter oauth feature
    Install Bird to wrap twitter API
    crated handlers for twitter OAUTH

--- server.js
+++ server.js
@@ -5,39 +5,84 @@
 
 var express = require('express'),
 	routes = require('./routes');
-	fs = require('fs');
+	fs = require('fs'),
+	Bird = require('bird')({
+		oauth_token : '346918539-bYQJZIivJU3jPaKElW3ELMJNCMpvuZNXEFqN5gpi',
+		oauth_token_secret : 'eNuPlNUAgJlGu9yF5dn0K5y0BOp9xyLqgNuncFZU',
+		callback: '/callback'
+	});
 
 // shorthand server
 var app = express.createServer();
 
 // Configuration
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({secret: "just-the-letter-a"}));
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 // Routes
 app.get('/', function(req, res) {
-  res.render('index', {
-  	title: 'getUserMedia',
-  	user: 'olu'
-  });
+	if (req.session.signedIn) {
+		res.send("Hi " + req.session.screen_name + " it's nice to see you signed in");
+	} else {
+		res.send('<a href="/login">login</a>');
+	}
+});
+
+app.get('/login', function(req, res) {
+	Bird.login(req, function(err, oauth_token, oauth_token_secret, results){
+		if (err) {
+			//handle the error here if twitter returns an error
+			res.send(err);
+		} else {
+			//set 
+			req.session.oauth_token = oauth_token;
+			req.session.oauth_token_secret = oauth_token_secret;
+			res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauth_token);
+		}
+	});
+});
+
+app.get('/callback', function(req,res) {
+	Bird.auth_callback(req, function(err, access_token, access_token_secret, data){
+		if (err) {
+			//handle the error here if twitter returns an error
+			res.send(err);
+		} else {
+			req.session.screen_name = data.screen_name;
+			req.session.access_token = access_token;
+			req.session.access_token_secret = access_token_secret;
+			req.session.signedIn = 1;
+
+			res.redirect('/');
+		}
+	});
+});
+
+app.get('/studio', function(req, res) {
+	res.render('index', {
+		title: 'getUserMedia',
+		user: 'olu'
+	});
 });
 
 app.get('/please', function(req, res) {
-  res.render('please', { title: 'Please' });
+	res.render('please', { title: 'Please' });
 });
 
 app.post('/snap', function(req, res) {
@@ -63,6 +108,10 @@ app.post('/snap', function(req, res) {
 			}
 		});
 	});
+});
+
+app.post('/1/statuses/update_with_media.json', function() {
+
 });
 
 
