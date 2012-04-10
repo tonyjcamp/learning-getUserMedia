d1233aea9da2a8c6fd445c1bd245ec0139e7494c
Tony Camp <tonyjcamp@gmail.com>
2012-04-10 15:16:02 -0700

    what now

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/snap', function(req, res) {
 	var imageData = '';
 
 	req.on('data', function(chunk) {
-		imageData = chunk;
+		imageData += chunk;
 	});
 
 	req.on('end', function() {
@@ -98,7 +98,7 @@ app.post('/snap', function(req, res) {
 	
 
 		var options = {
-			'status': 'Testing Crrnt Stts with pic',
+			'status': 'Testing Crrnt Stts, again',
 			'media[]': imageData
 		};
 
