95d58eb6ce923f4999fdf4478f3dfc6f72c6c306
Tony Camp <tonyjcamp@gmail.com>
2012-04-10 15:53:07 -0700

    it's fun building with friends

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/snap', function(req, res) {
 	
 
 		var options = {
-			'status': 'Testing Crrnt Stts with pic omg',
+			'status': 'Testing, Crrnt Stts is good',
 			'media[]': imageData
 		};
 
